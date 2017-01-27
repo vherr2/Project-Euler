75d4bd2d72eaf9aa43b0a941ab52f002d0ea33f2
Vincent Herr <vherr@OPED.local>
2017-01-27 00:35:30 -0600

    rewriting p1 for functions to be assigned to vars as function pointers

--- javascript/p1.js
+++ javascript/p1.js
@@ -1,17 +1,23 @@
-function flatten(arr) {
-	return arr.reduce((a, b) => a.concat(Array.isArray(b) ? flatten(b) : b), []);
-}
+var flatten = (function(arr) {
+	return arr.reduce(
+		(a, b) =>
+			a.concat(Array.isArray(b) ? flatten(b) : b), []);
+});
 
-function mults(n, start, max) {
+var mults = (function(n, start, max) {
 	return [...Array(max).keys()]
-		.slice(start)
-		.filter((num) => { return !(num%n) });
-}
+			.slice(start)
+			.filter((num) => { return !(num%n) });
+});
 
-function multsArr(arr, start, max) {
-	return [...new Set(flatten(arr.map((n) => mults(n, start, max))))]
-		.reduce((a, b) => { return a + b; });
-}
+var multsArr = (function(arr, start, max) {
+	return [...new Set(
+				flatten(arr.map(
+					(n) => mults(n, start, max)
+				))
+			)]
+			.reduce((a, b) => { return a + b; });
+});
 
 // sum of all multiples of 3 or 5 up to 1000
 console.log(multsArr([3, 5], 0, 1000));
