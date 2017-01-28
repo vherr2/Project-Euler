e5ecbf7e2ca43d76519fb618406d1c71dc937daf
Vincent Herr <vherr@OPED.local>
2017-01-28 13:59:12 -0600

    Changing p4.js to use max var instead of max(array)

--- javascript/p4.js
+++ javascript/p4.js
@@ -5,20 +5,20 @@ const isPalindrome = (num) => {
 const largestPalindrome = (numDigits) => {
 	let maxNum = parseInt('9'.repeat(numDigits));
 	let minNum = parseInt('1' + '0'.repeat(numDigits-1));
-	let palindrome = []
+	let maxPalindrome = 0
 
 	factors = [...Array(maxNum-minNum+1).keys()].map((n) => (n+minNum));
 
 	for (i of factors) {
 		for (j of factors) {
 			prod = i * j;
-			if (isPalindrome(prod)) {
-				palindrome.push(prod);
+			if (isPalindrome(prod) && prod > maxPalindrome) {
+				maxPalindrome = prod;
 			}
 		}
 	}
 
-	return Math.max(...palindrome);
+	return maxPalindrome;
 };
 
 // Return the largest palindrome that is a product of two 3-digit numbers
