e762130c01e81487a3cd6d6c2e27fac65dbe57a7
Vincent Herr <vherr@OPED.local>
2017-01-28 13:08:57 -0600

    Changing syntax on p1

--- javascript/p1.js
+++ javascript/p1.js
@@ -1,23 +1,23 @@
-var flatten = (function(arr) {
+const flatten = (arr) => {
 	return arr.reduce(
 		(a, b) =>
 			a.concat(Array.isArray(b) ? flatten(b) : b), []);
-});
+};
 
-var mults = (function(n, start, max) {
+const mults = (n, start, max) => {
 	return [...Array(max).keys()]
 			.slice(start)
 			.filter((num) => { return !(num%n) });
-});
+};
 
-var multsArr = (function(arr, start, max) {
+const multsArr = (arr, start, max) => {
 	return [...new Set(
 				flatten(arr.map(
 					(n) => mults(n, start, max)
 				))
 			)]
 			.reduce((a, b) => { return a + b; });
-});
+};
 
 // sum of all multiples of 3 or 5 up to 1000
 console.log(multsArr([3, 5], 0, 1000));
