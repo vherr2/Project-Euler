0547d6ce28e1381558fb31a7a11feb572c6300b0
Vincent Herr <vherr@OPED.local>
2017-01-28 15:41:15 -0600

    Making p5 solution recursive

--- python/p5.py
+++ python/p5.py
@@ -1,19 +1,22 @@
-def factorize(num, factors):
-	for i in factors:
-		if not num%i:
-			num /= i
-			if num == 1:
-				return factors
-	factors.append(num)
-	return factors
+def factorize(lim):
+	if lim < 1:
+		return None
+	elif lim == 1:
+		return [1]
+
+	factor_list = factorize(lim-1)
+
+	cur_num = lim
+	for i in factor_list:
+		cur_num = cur_num if cur_num % i else cur_num / i
+
+	if cur_num > 1:
+		factor_list.append(cur_num)
+
+	return factor_list
 
 def minProduct(lim):
-	factors = []
-	for i in xrange(2, lim):
-		temp = i
-		factors = factorize(i, factors)
-	return reduce(lambda x, y: x * y, factors)
+	return reduce(lambda x, y: x * y, factorize(lim))
 
 # minimum number that is divisible by all positive integers up to 20
 print minProduct(20)
-		
