4c54168861baf8faf239e9dd818063fb25ad62de
Vincent Herr <vherr@OPED.local>
2017-01-28 13:13:50 -0600

    Converting while loop to for loop in p2.js

--- javascript/p2.js
+++ javascript/p2.js
@@ -3,16 +3,13 @@ const memo = [1, 1]
 const fib = (n) => {
 	if (memo[n]) return memo[n];
 
-	m = memo.length;
-
-	while (m < n) {
+	for (let m = memo.length; m < n; m++) {
 		memo.push(
 			memo.slice(-2)
 				.reduce((a, b) => { return a + b; })
 		);
-		m = memo.length;
 	}
-}
+};
 
 const fib_even_sum = (lim) => {
 	while (memo.slice(-1) < lim) {
@@ -22,7 +19,7 @@ const fib_even_sum = (lim) => {
 	return memo.slice(0, -1)
 				.filter((n) => { return !(n%2); })
 				.reduce((a, b) => { return a + b; })
-}
+};
 
 // Sum of all even fibonacci numbers up to 4 million
 console.log(fib_even_sum(4*Math.pow(10, 6)))
