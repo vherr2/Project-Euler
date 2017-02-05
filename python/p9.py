055ee1bc7abc131f49b3cc35bec946b34461350b
Vincent Herr <vherr@OPED.local>
2017-02-05 13:17:20 -0600

    Removing outermost loop in p9.py function to reduce complexity

--- python/p9.py
+++ python/p9.py
@@ -1,6 +1,7 @@
 def pythagorean_triplet(total):
-	return [x*y*z for z in xrange(total/3, total-1) 
-				for y in xrange(total/6, z) 
-				for x in xrange(1, y) if (x**2 + y**2 == z**2) and (x + y + z == total)]
+	return [x*y*((x**2 + y**2)**0.5)
+				for y in xrange(2, total)
+				for x in xrange(1, y) if x+y+((x**2 + y**2)**0.5) == total]
 
+# Product of sides of pythagorean triplet for which sum of sides is 1000
 print pythagorean_triplet(1000)
