0cd83f71b91961b4f960bba00e838d8fea1dbed9
Vincent Herr <vherr@OPED.local>
2017-02-05 14:10:23 -0600

    Adding solution for p10 in JS

---
+++ javascript/10.js
@@ -0,0 +1,23 @@
+const isPrime = (num, sieve) => {
+	let idx = 0;
+
+	for (let prime of sieve) {
+		if (num % prime === 0) { return false; }
+		if (Math.pow(prime, 2) > num) { return true; }
+	}
+};
+
+const primeSieve = (lim) => {
+	let sieve = [2];
+	let num = 3;
+
+	while (num < lim) {
+		if (isPrime(num, sieve)) { sieve.push(num); }
+		num += 2;
+	}
+
+	return sieve.reduce((x, y) => { return x + y; });
+};
+
+// Calculate 10,001st prime number
+console.log(primeSieve(2*Math.pow(10, 6)));
