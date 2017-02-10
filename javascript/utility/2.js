08d491978cb7f9a8281c978da2c39a72ed85ef40
vherr2 <vherr@OPED.local>
2017-02-10 07:25:31 -0600

    Simplifying logic in P2 with lodash

--- javascript/utility/2.js
+++ javascript/utility/2.js
@@ -3,18 +3,15 @@ var sum = require('lodash/sum');
 
 const memo = [1, 1]
 
-const fib = (n) => {
-	if (memo[n]) return memo[n];
-	memo.push(sum(memo.slice(-2)));
-};
-
 const fib_even_sum = (lim) => {
 	while (last(memo) < lim) {
-		fib(memo.length+1);
+		memo.push(sum(memo.slice(-2)));
 	}
 	
 	return sum(memo.filter((n) => { return !(n%2); }))
 };
+
+
 
 // Sum of all even fibonacci numbers up to 4 million
 console.log(fib_even_sum(4*Math.pow(10, 6)))
